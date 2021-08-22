7ff0aaf00f6b25fd2dce46a334cd7d6b54bcad58
vivi munjia <vivisangadji06@gmail.com>
2021-08-22 09:30:33 -0700

    Menambahkan file app.js

---
+++ app.js
@@ -0,0 +1,77 @@
+const path = require('path');
+const hbs = require('hbs');
+const bodyParser = require('body-parser');
+const mysql = require('mysql');
+const express = require('express');
+
+const app = express();
+
+// konfigurasi koneksi
+const conn = mysql.createConnection({
+	host: 'localhost',
+	user: 'root',
+	password: '',
+	database: 'crud_nodejs'
+});
+
+// Connect database
+conn.connect((err) =>{
+	if(err) throw err;
+	console.log('Mysql Connected...');
+});
+
+// set views file
+app.set('views', path.join(__dirname, 'views'));
+// set view engine
+app.set('view engine', 'hbs');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+//set folder public sebagai static folder untuk static file
+app.use('/assets', express.static(__dirname + '/public'));
+
+// route untuk tampil data
+app.get('/',(req, res) => {
+	let sql = "SELECT * FROM product";
+	let query = conn.query(sql, (err, results) => {
+		if(err) throw err;
+		res.render('product_view', {
+			results : results
+		});
+	});
+});
+
+// route untuk create data
+app.post('/save', (req, res) => {
+	let data = {product_name: req.body.product_name, product_price: req.body.product_price};
+	let sql = "INSERT INTO product SET ?";
+	let query = conn.query(sql, data, (err, results) => {
+		if(err) throw err;
+    	res.redirect('/');
+	});
+});
+
+// route untuk update data
+app.post('/update', (req, res) =>{
+	let sql = "UPDATE product SET product_name='"+req.body.product_name+"',product_price='"+req.body.product_price+"' WHERE product_id="+req.body.id+"";
+	let query = conn.query(sql, (err, results) => {
+	if(err) throw err;
+    res.redirect('/');
+	});
+});
+
+//route untuk delete data
+app.post('/delete',(req, res) => {
+  let sql = "DELETE FROM product WHERE product_id="+req.body.product_id+"";
+  let query = conn.query(sql, (err, results) => {
+    if(err) throw err;
+      res.redirect('/');
+  });
+});
+
+
+//server listening
+const port = 3000;
+
+app.listen(port, () => {
+  console.log(`listening at http://localhost:${port}`)
+});
